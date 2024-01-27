aeb342ccd144f09dd09a2261452e118f1642e188
W4terF4ll <56806946+W4terF4ll@users.noreply.github.com>
2024-01-27 00:35:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,55 +1,135 @@
-var noteCount = 1;
-var noteSpacing = 250;
 let patternList = [
-	{
-		pattern: [1, 2, 3, 5, 1, 4, 3, 2, 1]
+	{	
+		type: 0,
+		pattern: [
+			[]
+		]
 	},
 	{
-		pattern: [1, 4, 1, 4]
+		type: 1,
+		pattern: [
+			[1, 2, 3, 4],
+			[4, 3, 1, 2],
+			[1, 2, 3],
+			[1, 4, 1, 4],
+			[2, 3, 2, 3],
+			[1, 4, 2, 3],
+			[2, 3, 4, 1, 2, 3],
+			[1, 2, 3, 2, 1],
+			[4, 3, 2, 4, 2, 3, 4]
+		]
 	},
 	{
-		pattern: [4, 3, 2, 1]
+		type: 2,
+		pattern: [
+			[2, 3, 5, 4, 1, 2, 3, 5, 4, 1],
+			[1, 2, 3, 5, 4, 1, 2, 3, 5, 4, 1],
+			[1, 1],
+			[3, 3],
+			[1, 1, 4, 4],
+			[2, 2, 3, 3],
+			[1, 2, 3, 5, 1, 4, 3, 2, 1],
+			[4, 5, 2, 3, 1, 5, 2, 3, 4],
+			
+		]
 	},
 	{
-		pattern: [1, 2, 4, 3]
+		type: 3,
+		pattern: [
+			[5, 1, 2, 3, 4, 3, 5, 1, 2],
+			[5, 1, 4, 5, 1, 4],
+			[5, 2, 3, 5, 2, 3],
+			[1, 6, 2, 3, 4, 4, 6, 4, 3, 2, 1],
+			[5, 1, 3, 2, 5, 2, 4, 3],
+		]
 	},
 	{
-		pattern: [5, 1, 2, 5, 3, 4]
+		type: 4,
+		pattern: [
+			[7, 1, 2, 3, 4],
+			[6, 1, 2, 3],
+			[6, 1, 3, 4],
+			[5, 1, 2, 5, 2, 3, 5, 2, 4, 5, 1, 4, 5, 1, 3],
+			[5, 1, 2, 5, 3, 4, 5, 1, 2, 5, 3, 4],
+			[5, 1, 4, 5, 2, 3, 5, 1, 4, 5, 2, 3],
+			[5, 1, 3, 5, 2, 4, 5, 1, 3, 5, 2, 4],
+		]
 	},
-	{
-		pattern: [5, 2, 4, 5, 1, 3, 5, 2, 4]
-	},
-	{
-		pattern: [5, 1, 4, 5, 2, 3]
-	},
-	{
-		pattern: [6, 1, 2, 3]
-	},
-	{
-		pattern: [6, 4, 3, 2]
-	}
 ];
+
+// running vars
 var isCharting;
+var isRunning = false;
+
+// pattern vars
 var randomPattern = 0;
 var patternProgress = 0;
 var patternLength = 0;
 var currentPattern = [1];
 var patternHolder = [1];
+
+// scoring vars
+var noteCount = 1;
 var noteCombo = 0;
 var noteScore = 0;
-document.onload = function() {
-	var button1 = document.getElementById("button1");
-	var button2 = document.getElementById("button2");
-	var button3 = document.getElementById("button3");
-	var button4 = document.getElementById("button3");
-	var lane1 = document.getElementById("lane1");
-	var lane2 = document.getElementById("lane2");
-	var lane3 = document.getElementById("lane3");
-	var lane4 = document.getElementById("lane4");
-	var combo = document.getElementById("combo");
-	var judge = document.getElementById("judge");
-	var score = document.getElementById("score");
+var bestScore = 0;
+
+// difficulty vars
+var lives = 9;
+var startingSpacing = 150;
+var noteSpacing = startingSpacing;
+var spaceDifficulty = 0;
+var spaceIncrease = 100;
+var startingDifficulty = 1;
+var noteDifficulty = startingDifficulty;
+var increaseCount = 0;
+var noteIncrease = 3;
+var randomType = Math.floor(Math.random() * noteDifficulty) + 1;
+
+// dom vars
+var button1;
+var button2
+var button3;
+var button4;
+var lane1;
+var lane2;
+var lane3;
+var lane4;
+var combo;
+var judge;
+var score;
+var highScore;
+var cssRoot;
+var lifeList;
+var stage;
+var audio = new Array(10);
+var audioMiss;
+var audioIndex = 0;
+
+// sets dom vars on window load
+window.onload = function() {
+	cssRoot = document.documentElement;
+	button1 = document.getElementById("button1");
+	button2 = document.getElementById("button2");
+	button3 = document.getElementById("button3");
+	button4 = document.getElementById("button4");
+	lane1 = document.getElementById("lane1");
+	lane2 = document.getElementById("lane2");
+	lane3 = document.getElementById("lane3");
+	lane4 = document.getElementById("lane4");
+	lifeList = document.getElementsByClassName('lives');
+	combo = document.getElementById("combo");
+	judge = document.getElementById("judge");
+	score = document.getElementById("score");
+	highScore = document.getElementById("highScore");
+	stage = document.getElementById("stage");
+	for (var i = 0; i < audio.length; i++) {
+		audio[i] = new Audio("hit.wav");
+	}
+	audioMiss = new Audio("miss.wav");
 }
+
+// records key presses
 document.addEventListener("keypress", function onEvent(event) {
 	if (event.key === "a") {
 		button1.style.backgroundColor = "#7dfffb";
@@ -74,6 +154,7 @@ document.addEventListener("keypress", function onEvent(event) {
 	}
 });
 
+// records key releases
 document.addEventListener("keyup", function onEvent(event) {
 	if (event.key === "a") {
 		button1.style.backgroundColor = "#d9d9d7";
@@ -90,79 +171,161 @@ document.addEventListener("keyup", function onEvent(event) {
 	}
 });
 
+// plays hit sound
+function playSound() {
+    audio[audioIndex].play();
+    audioIndex++;
+    if(audioIndex > audio.length - 1) {
+      audioIndex = 0;
+    }
+  }
+
+// judges accuracy, updates combo & score
 function judgment(note, dist) {
 	if (dist < 15) {
 		noteCombo += 1;
 		noteScore += 300 * (1 + (noteCombo / 100));
 		judge.style.color = "#ffff33";
 		judge.innerText = "PERFECT!!";
+		playSound();
 		note.remove();
 	} else if (dist < 30) {
 		noteCombo += 1;
 		noteScore += 200 * (1 + (noteCombo / 100));
 		judge.style.color = "#70dbdb";
 		judge.innerText = "Great!";
+		playSound();
 		note.remove();
 	} else if (dist < 45) {
 		noteCombo += 1;
 		noteScore += 100 * (1 + (noteCombo / 100));
 		judge.style.color = "#1aff1a";
 		judge.innerText = "Good";
+		playSound();
 		note.remove();
 	} else if (dist < 70) {
 		noteCombo += 1;
 		noteScore += 50 * (1 + (noteCombo / 100));
 		judge.style.color = "#ff3385";
-		judge.innerText = "Bad..";
+		judge.innerText = "Bad...";
+		playSound();
 		note.remove();
 	} else {
+		audioMiss.play();
 		noteCombo = 0;
 		judge.style.color = "#ff0000";
 		judge.innerText = "MISS";
+		lifeLost();
 	}
+	judge.classList.remove("bounce");
+	combo.classList.remove("bounce");
+	judge.scrollBy(0, 0);
+	combo.scrollBy(0, 0);
+	judge.classList.add("bounce");
+	combo.classList.add("bounce");
 	combo.innerText = noteCombo;
-	score.innerText = noteScore;
+	score.innerText = Math.round(noteScore);
 }
 
+// subtracts lives
+function lifeLost() {
+	if (isRunning) {
+		lifeList[lives-1].style.display = "none";
+		lives -= 1;
+		if (lives == 0) {
+			gameOver();
+		}
+	}
+}
+
+// resets game
+function gameOver() {
+	isRunning = false;
+	var allNotes = document.getElementsByClassName('note');
+	var allLength = allNotes.length;
+	for (var i = 0; i < allLength; i++) {
+		allNotes[0].remove();
+	}
+	stopCharting();
+	if (noteScore > bestScore) {
+		bestScore = noteScore;
+	}
+	noteScore = 0;
+	score.innerText = Math.round(noteScore);
+	highScore.innerText = Math.round(bestScore);
+	for (var i = 0; i < lifeList.length; i++) {
+		lifeList[i].style.display = "block";
+	}
+	noteSpacing = startingSpacing;
+	noteDifficulty = startingDifficulty;
+	spaceDifficulty = 0;
+	increaseCount = 0;
+	stage.innerText = noteDifficulty;
+	lives = 9;
+}
+
+// starts the charter
 function startCharting() {
-	randomPattern = Math.floor(Math.random() * (patternList.length));
-	currentPattern = patternList[randomPattern].pattern;
-	patternHolder = currentPattern;
-	patternLength = patternList[randomPattern].pattern.length;
+	isRunning = true;
+	patternSelector();
 	clearInterval(isCharting);
 	isCharting = setInterval(function(){autoCharter()}, noteSpacing);
 }
 
+// selects patterns
+function patternSelector() {
+	randomType = Math.floor(Math.random() * noteDifficulty) + 1;
+	randomPattern = Math.floor(Math.random() * (patternList[randomType].pattern.length));
+	currentPattern = patternList[randomType].pattern[randomPattern];
+	patternHolder = currentPattern;
+	patternLength = currentPattern.length;
+}
+
+// stops the charter
 function stopCharting() {
 	clearInterval(isCharting);
 }
 
+// reads patterns
+function noteLogic() {
+	if (patternHolder[patternProgress] == 5) {
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+	} else if (patternHolder[patternProgress] == 6) {
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+	} else if (patternHolder[patternProgress] == 7) {
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+	} else {
+		spawnNote(patternHolder[patternProgress]);
+		patternProgress += 1;
+	}
+}
+
+// displays patterns, manages difficulty
 function autoCharter() {
 	if (patternProgress < patternLength) {
-		if (patternHolder[patternProgress] == 5) {
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-		} else if (patternHolder[patternProgress] == 6) {
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-		} else {
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-		}
+		noteLogic();
 	} else {
 		patternProgress = 0;
 		var mirrorFactor = Math.floor(Math.random() * 100) + 1;
-		randomPattern = Math.floor(Math.random() * (patternList.length));
-		currentPattern = patternList[randomPattern].pattern;
-		patternHolder = currentPattern;
+		patternSelector();
 		if (mirrorFactor > 50) {
 			var mirrorHolder = [];
 			for (i = 0; i < currentPattern.length; i++) {
@@ -180,45 +343,44 @@ function autoCharter() {
 			}
 			patternHolder = mirrorHolder;
 		}
-		patternLength = patternList[randomPattern].pattern.length;
-		if (patternHolder[patternProgress] == 5) {
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-		} else if (patternHolder[patternProgress] == 6) {
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
-		} else {
-			spawnNote(patternHolder[patternProgress]);
-			patternProgress += 1;
+		noteLogic();
+	}
+	spaceDifficulty += 1;
+	if (spaceDifficulty >= spaceIncrease) {
+		spaceDifficulty = 0;
+		noteSpacing = noteSpacing * 0.9;
+		increaseCount += 1;
+		if (increaseCount >= noteIncrease && noteDifficulty < 5) {
+			noteSpacing = startingSpacing;
+			increaseCount = 0;
+			noteDifficulty += 1;
+			stage.innerText = noteDifficulty;
 		}
 	}
 	clearInterval(isCharting);
 	isCharting = setInterval(function(){autoCharter()}, noteSpacing);
 }
 
+// spawns a note on a set lane
 function spawnNote(num) {
 	var lane = document.getElementById("lane" + num);
 	var divTest = document.createElement("div");
-	divTest.innerHTML = "<div class='note' id='note"+noteCount+"'></div>";
+	divTest.className = "note";
+	divTest.setAttribute("id", "note" + noteCount);
 	divTest.addEventListener('animationend', () => {
+		audioMiss.play();
 		noteCombo = 0;
 		judge.style.color = "#ff0000";
 		judge.innerText = "MISS";
 		combo.innerText = noteCombo;
 		divTest.remove();
+		lifeLost();
 	});
 	lane.appendChild(divTest);
 	noteCount += 1;
 }
 
+// finds the center of an element
 function getPositionAtCenter(element) {
 	const {top, left, width, height} = element.getBoundingClientRect();
 	return {
@@ -226,12 +388,15 @@ function getPositionAtCenter(element) {
 	   y: top + height / 2,
 	};
  }
+ 
+ // calculates distance from note to button
  function getDistanceBetweenElements(a, b) {
 	 const aPosition = getPositionAtCenter(a);
 	 const bPosition = getPositionAtCenter(b);
 	 return Math.hypot(aPosition.x - bPosition.x, aPosition.y - bPosition.y);  
 }
 
+// gets closest note to the button
 function getClosestElement(a, b) {
 	const matches = a.querySelectorAll("div.note")
 	var distance = Number.MAX_VALUE;
